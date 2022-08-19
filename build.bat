f91eff893891460ffe8a29fbc2dad0ef0ec75cf4
RabbitLabs <40416043+RabbitLabs@users.noreply.github.com>
2022-08-19 05:33:43 +0200

    ignore some files,, build command and launch command for VS Code

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 SET GOOS=linux
 SET GOARCH=amd64
 go build -o server_amd64
-docker build . -t rabbitlabs/thumperproxy
-docker login
-docker push rabbitlabs/thumperproxy
+SET GOOS=windows
+go build
+
